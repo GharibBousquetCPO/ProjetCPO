53832395917205604c4f1ea0a1007812ffab0371
elea <elea@192.168.1.25>
2020-11-04 11:21:49 +0100

    nouveau fichier

--- src/superpuissance4_guttadoro_prouillet/Grille.java
+++ src/superpuissance4_guttadoro_prouillet/Grille.java
@@ -96,6 +96,7 @@ public String lireCouleurDuJeton(int ligne, int colonne){
     //renvoie la couleur du jeton de la cellule ciblée
    return Cellules[ligne][colonne].recupererJeton().lireCouleur();
    
+   
 }       
  
 /* public boolean  etreGagnantePourJoueur(Joueur){
